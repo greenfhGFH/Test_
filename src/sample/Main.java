a47f01d24bdafbc592435b8ae215bd2f174a097f
greenfhGFH <greenfh@gmail.com>
2015-04-23 20:18:09 +0300

    Fix issue PM #28

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Test");
         primaryStage.setFullScreen(true);
-        primaryStage.setMaximized(true);
+        //primaryStage.setMaximized(true);
         primaryStage.setScene(new Scene(root, 300, 275));
 
         primaryStage.show();

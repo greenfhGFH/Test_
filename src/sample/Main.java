61c7673b5f87551a159435c7736e97964afbeaf9
greenfhGFH <greenfh@gmail.com>
2015-04-23 20:02:44 +0300

    Fix issue PM #28

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,8 +11,11 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Test ");
+        primaryStage.setTitle("Test");
+        primaryStage.setFullScreen(true);
+        primaryStage.setMaximized(true);
         primaryStage.setScene(new Scene(root, 300, 275));
+
         primaryStage.show();
     }
 
